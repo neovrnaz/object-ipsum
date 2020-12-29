74a8531e3e6920f4463e1fc60eed6339680e41cb
neovrnaz <neovrnaz@gmail.com>
2020-12-29 02:05:48 -0700

    Add more objects

--- src/_data/objects.js
+++ src/_data/objects.js
@@ -17,9 +17,14 @@ const truck = {
 };
 
 const person = {
-  name: 'George',
-  age: 27,
-  eyeColor: 'Hazel',
+  firstName: 'James',
+  lastName: 'Bond',
+  age: 25,
+  eyeColor: 'Blue-grey',
+  address: {
+    id: 1,
+    country: 'UK',
+  },
 };
 
 const school = {
@@ -28,7 +33,107 @@ const school = {
   established: '1971',
 };
 
-const array = [product, truck, person, school];
+const codes = {
+  49: 'Germany',
+  41: 'Switzerland',
+  44: 'Great Britain',
+  1: 'USA',
+};
+
+const domainExtensions = {
+  com: 'Commercial',
+  org: 'Organization',
+  net: 'Network',
+  co: 'Company',
+};
+
+const employee = {
+  firstName: 'Peter',
+  lastName: 'Doe',
+  employeeId: 1,
+};
+
+const student = {
+  name: 'Sheldon',
+  age: 20,
+  marks: {
+    science: 70,
+    math: 75,
+  },
+};
+
+const data = {
+  name: 'Eleven',
+  show: 'Stranger Things',
+  portrayedBy: 'Millie Bobby Brown',
+};
+
+const mobile = {
+  brand: 'Samsung',
+  model: 'Galaxy Note20',
+  year: 2020,
+};
+
+const classOf2022 = {
+  students: 35,
+  year: 2022,
+  averageAge: 25,
+};
+
+const rubiksCubeFacts = {
+  possiblePermutations: '43,252,003,274,489,856,000',
+  invented: '1974',
+  largestCube: '17x17x17',
+};
+
+const restaurant = {
+  numCustomers: 45,
+  seatCapacity: 100,
+  menu: {
+    item1: 'burger',
+    item2: 'fries',
+    item3: 'drink',
+  },
+};
+
+const dog = {
+  name: 'Buttons',
+  age: '7',
+  breed: 'BEARDED COLLIE',
+};
+
+const salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+  George: 170,
+};
+
+const address = {
+  'building no': 3960,
+  street: 'North 1st street',
+  state: 'CA',
+  country: 'USA',
+};
+
+const array = [
+  product,
+  truck,
+  person,
+  school,
+  codes,
+  domainExtensions,
+  employee,
+  student,
+  data,
+  mobile,
+  classOf2022,
+  rubiksCubeFacts,
+  restaurant,
+  dog,
+  salaries,
+  address,
+];
 
 module.exports = {
   array,
